d8084dfc6731f8d68e9569b2d09e8cb7a5ce8bee
AAA4002911 <zeeshan4002911@gmail.com>
2023-09-07 06:56:24 +0530

    soln added

---
+++ JavaStringReverse.java
@@ -0,0 +1,44 @@
+
+/**
+ * A palindrome is a word, phrase, number, or other sequence of characters which reads the same backward or forward.
+
+Given a string
+
+, print Yes if it is a palindrome, print No otherwise.
+
+Constraints
+
+will consist at most
+
+    lower case english letters.
+
+Sample Input
+
+madam
+
+Sample Output
+
+Yes
+
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class JavaStringReverse {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String A = sc.next();
+        sc.close();
+        /* Enter your code here. Print output to STDOUT. */
+        for (int i = 0, j = A.length() - 1; i < j; i++, j--) {
+            if (A.charAt(i) != A.charAt(j)) {
+                System.out.println("No");
+                return;
+            }
+        }
+        System.out.println("Yes");
+    }
+}
