7a3fcf024d4fb305fdc8d309832ba469eddbf903
zeesh <zeeshan4002911@gmail.com>
2025-05-22 08:36:48 +0530

    control flow added

--- java-essentials/Basic.java
+++ java-essentials/Basic.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -10,17 +11,17 @@ public class Basic {
         long years = 4543000000l;
         String name = "Zeesh";
         String origin = "Human";
-        float start_seconds = 10.99f;
+        float startSeconds = 10.99f;
 
         // String formatting
         String bio = String.format(
                 "I am %s, I am a %s and it's been %,d years of earth. Lets start in %.2f seconds",
-                name, origin, years, start_seconds);
+                name, origin, years, startSeconds);
         System.out.println(bio);
 
         // Scheduler for repeating task
         ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
-        final int[] count = {(int) Math.floor(start_seconds)};
+        final int[] count = { (int) Math.floor(startSeconds) };
         Runnable task = () -> {
             if (count[0] >= 0) {
                 // \r for output on same line
@@ -32,5 +33,39 @@ public class Basic {
         };
 
         scheduler.scheduleAtFixedRate(task, 1, 1, TimeUnit.SECONDS);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the score(%) to be graded: ");
+        final int scorePercentage = scanner.nextInt();
+        System.out.println("Grade: " + ControlFlow.getGrade(scorePercentage));
+        scanner.close();
+    }
+
+}
+
+class ControlFlow {
+    // Returns grade based on score percentage
+    public static String getGrade(int scorePercentage) {
+        String grade = null;
+        if (scorePercentage >= 90) {
+            grade = "O";
+        } else if (scorePercentage >= 75) {
+            grade = "A";
+        } else if (scorePercentage >= 50) {
+            grade = "B";
+        } else if (scorePercentage >= 25) {
+            grade = "C";
+        } else {
+            grade = "F";
+        }
+        return grade;
+    }
+
+    public void printEvenNumbers(int limit) {
+        for (int i = 1; i <= limit; i++) {
+            if (i % 2 == 0) {
+                System.out.println("Even number: " + i);
+            }
+        }
     }
 }
